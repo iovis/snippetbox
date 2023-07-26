e0f1ed3363e048c0b9bee2e89b0e5464cd8d7f32
David Marchante <davidmarchan@gmail.com>
2023-07-25 23:17:24 -0400

    Add a few more routes

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server in :4000")
 	err := http.ListenAndServe(":4000", mux)
@@ -16,4 +18,12 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a snippet"))
 }
