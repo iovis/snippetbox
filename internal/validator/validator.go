10faaa8c543208aa7702a20188a8e6f55ee3da05
David Marchante <davidmarchan@gmail.com>
2023-08-06 20:26:15 -0400

    Add Insert User to database

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -16,7 +16,7 @@ func (v *Validator) Valid() bool {
 	return len(v.FieldErrors) == 0
 }
 
-func (v *Validator) addFieldError(key, message string) {
+func (v *Validator) AddFieldError(key, message string) {
 	if v.FieldErrors == nil {
 		v.FieldErrors = make(map[string]string)
 	}
@@ -28,7 +28,7 @@ func (v *Validator) addFieldError(key, message string) {
 
 func (v *Validator) CheckField(ok bool, key, message string) {
 	if !ok {
-		v.addFieldError(key, message)
+		v.AddFieldError(key, message)
 	}
 }
 
