874ea816d87a3399ecb283deb42b0a304c5d32af
David Marchante <davidmarchan@gmail.com>
2024-03-28 20:50:58 -0400

    feat: create custom view function `humanDate`

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -20,6 +20,14 @@ func (self *application) newTemplateData(r *http.Request) templateData {
 	}
 }
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
@@ -31,7 +39,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
